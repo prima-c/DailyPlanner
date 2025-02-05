9e1a17bd5209dadcdfdf859b39a03448695b491f
prima-c <prima.changwatchai@gmail.com>
2025-02-05 10:24:14 -0600

    Created basic layout of see-day page design.

---
+++ DailyPlanner/SeeDayForm.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace DailyPlanner
+{
+    partial class SeeDayForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMainPage4 = new System.Windows.Forms.Button();
+            this.lblTitle3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMainPage4
+            // 
+            this.btnMainPage4.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnMainPage4.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMainPage4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnMainPage4.Location = new System.Drawing.Point(41, 585);
+            this.btnMainPage4.Name = "btnMainPage4";
+            this.btnMainPage4.Size = new System.Drawing.Size(312, 45);
+            this.btnMainPage4.TabIndex = 42;
+            this.btnMainPage4.Text = "Main Page";
+            this.btnMainPage4.UseVisualStyleBackColor = false;
+            // 
+            // lblTitle3
+            // 
+            this.lblTitle3.AutoSize = true;
+            this.lblTitle3.Font = new System.Drawing.Font("Courier New", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblTitle3.Location = new System.Drawing.Point(0, 33);
+            this.lblTitle3.Name = "lblTitle3";
+            this.lblTitle3.Size = new System.Drawing.Size(399, 50);
+            this.lblTitle3.TabIndex = 39;
+            this.lblTitle3.Text = "Daily Planner";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(41, 264);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(312, 45);
+            this.button1.TabIndex = 46;
+            this.button1.Text = "See more...";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(41, 224);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(312, 45);
+            this.button2.TabIndex = 47;
+            this.button2.Text = "Title";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button3.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button3.Location = new System.Drawing.Point(41, 327);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(312, 45);
+            this.button3.TabIndex = 49;
+            this.button3.Text = "Title";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button4.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button4.Location = new System.Drawing.Point(41, 367);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(312, 45);
+            this.button4.TabIndex = 48;
+            this.button4.Text = "See more...";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button5.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button5.Location = new System.Drawing.Point(41, 430);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(312, 45);
+            this.button5.TabIndex = 51;
+            this.button5.Text = "Title";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button6.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button6.Location = new System.Drawing.Point(41, 470);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(312, 45);
+            this.button6.TabIndex = 50;
+            this.button6.Text = "See more...";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button7.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button7.Location = new System.Drawing.Point(41, 119);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(312, 45);
+            this.button7.TabIndex = 52;
+            this.button7.Text = "Date";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // SeeDayForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 706);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnMainPage4);
+            this.Controls.Add(this.lblTitle3);
+            this.Name = "SeeDayForm";
+            this.Text = "SeeDayForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnMainPage4;
+        private System.Windows.Forms.Label lblTitle3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
