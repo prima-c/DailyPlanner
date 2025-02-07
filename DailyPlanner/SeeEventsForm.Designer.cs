bd4898e1c6a84a6bda961cdc5b5db3bc96476e00
prima-c <prima.changwatchai@gmail.com>
2025-02-07 10:02:00 -0600

    Finished event list page design.

---
+++ DailyPlanner/SeeEventsForm.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace DailyPlanner
+{
+    partial class SeeEventsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChangeDate5 = new System.Windows.Forms.Button();
+            this.btnTitleThree6 = new System.Windows.Forms.Button();
+            this.btnSeeMoreThree6 = new System.Windows.Forms.Button();
+            this.btnTitleTwo6 = new System.Windows.Forms.Button();
+            this.btnSeeMoreTwo6 = new System.Windows.Forms.Button();
+            this.btnTitleOne6 = new System.Windows.Forms.Button();
+            this.btnSeeMoreOne6 = new System.Windows.Forms.Button();
+            this.btnMainPage6 = new System.Windows.Forms.Button();
+            this.lblTitle6 = new System.Windows.Forms.Label();
+            this.lblUpcomingEvents = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnChangeDate5
+            // 
+            this.btnChangeDate5.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnChangeDate5.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChangeDate5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnChangeDate5.Location = new System.Drawing.Point(41, 141);
+            this.btnChangeDate5.Name = "btnChangeDate5";
+            this.btnChangeDate5.Size = new System.Drawing.Size(0, 0);
+            this.btnChangeDate5.TabIndex = 70;
+            this.btnChangeDate5.Text = "Date";
+            this.btnChangeDate5.UseVisualStyleBackColor = false;
+            // 
+            // btnTitleThree6
+            // 
+            this.btnTitleThree6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnTitleThree6.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTitleThree6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnTitleThree6.Location = new System.Drawing.Point(41, 412);
+            this.btnTitleThree6.Name = "btnTitleThree6";
+            this.btnTitleThree6.Size = new System.Drawing.Size(312, 45);
+            this.btnTitleThree6.TabIndex = 69;
+            this.btnTitleThree6.Text = "Title";
+            this.btnTitleThree6.UseVisualStyleBackColor = false;
+            // 
+            // btnSeeMoreThree6
+            // 
+            this.btnSeeMoreThree6.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnSeeMoreThree6.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSeeMoreThree6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnSeeMoreThree6.Location = new System.Drawing.Point(41, 452);
+            this.btnSeeMoreThree6.Name = "btnSeeMoreThree6";
+            this.btnSeeMoreThree6.Size = new System.Drawing.Size(312, 45);
+            this.btnSeeMoreThree6.TabIndex = 68;
+            this.btnSeeMoreThree6.Text = "See more...";
+            this.btnSeeMoreThree6.UseVisualStyleBackColor = false;
+            // 
+            // btnTitleTwo6
+            // 
+            this.btnTitleTwo6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnTitleTwo6.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTitleTwo6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnTitleTwo6.Location = new System.Drawing.Point(41, 309);
+            this.btnTitleTwo6.Name = "btnTitleTwo6";
+            this.btnTitleTwo6.Size = new System.Drawing.Size(312, 45);
+            this.btnTitleTwo6.TabIndex = 67;
+            this.btnTitleTwo6.Text = "Title";
+            this.btnTitleTwo6.UseVisualStyleBackColor = false;
+            // 
+            // btnSeeMoreTwo6
+            // 
+            this.btnSeeMoreTwo6.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnSeeMoreTwo6.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSeeMoreTwo6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnSeeMoreTwo6.Location = new System.Drawing.Point(41, 349);
+            this.btnSeeMoreTwo6.Name = "btnSeeMoreTwo6";
+            this.btnSeeMoreTwo6.Size = new System.Drawing.Size(312, 45);
+            this.btnSeeMoreTwo6.TabIndex = 66;
+            this.btnSeeMoreTwo6.Text = "See more...";
+            this.btnSeeMoreTwo6.UseVisualStyleBackColor = false;
+            // 
+            // btnTitleOne6
+            // 
+            this.btnTitleOne6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnTitleOne6.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTitleOne6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnTitleOne6.Location = new System.Drawing.Point(41, 206);
+            this.btnTitleOne6.Name = "btnTitleOne6";
+            this.btnTitleOne6.Size = new System.Drawing.Size(312, 45);
+            this.btnTitleOne6.TabIndex = 65;
+            this.btnTitleOne6.Text = "Title";
+            this.btnTitleOne6.UseVisualStyleBackColor = false;
+            // 
+            // btnSeeMoreOne6
+            // 
+            this.btnSeeMoreOne6.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnSeeMoreOne6.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSeeMoreOne6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnSeeMoreOne6.Location = new System.Drawing.Point(41, 246);
+            this.btnSeeMoreOne6.Name = "btnSeeMoreOne6";
+            this.btnSeeMoreOne6.Size = new System.Drawing.Size(312, 45);
+            this.btnSeeMoreOne6.TabIndex = 64;
+            this.btnSeeMoreOne6.Text = "See more...";
+            this.btnSeeMoreOne6.UseVisualStyleBackColor = false;
+            // 
+            // btnMainPage6
+            // 
+            this.btnMainPage6.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnMainPage6.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMainPage6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnMainPage6.Location = new System.Drawing.Point(41, 607);
+            this.btnMainPage6.Name = "btnMainPage6";
+            this.btnMainPage6.Size = new System.Drawing.Size(312, 45);
+            this.btnMainPage6.TabIndex = 63;
+            this.btnMainPage6.Text = "Main Page";
+            this.btnMainPage6.UseVisualStyleBackColor = false;
+            // 
+            // lblTitle6
+            // 
+            this.lblTitle6.AutoSize = true;
+            this.lblTitle6.Font = new System.Drawing.Font("Courier New", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblTitle6.Location = new System.Drawing.Point(0, 55);
+            this.lblTitle6.Name = "lblTitle6";
+            this.lblTitle6.Size = new System.Drawing.Size(399, 50);
+            this.lblTitle6.TabIndex = 62;
+            this.lblTitle6.Text = "Daily Planner";
+            // 
+            // lblUpcomingEvents
+            // 
+            this.lblUpcomingEvents.AutoSize = true;
+            this.lblUpcomingEvents.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUpcomingEvents.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblUpcomingEvents.Location = new System.Drawing.Point(63, 141);
+            this.lblUpcomingEvents.Name = "lblUpcomingEvents";
+            this.lblUpcomingEvents.Size = new System.Drawing.Size(269, 30);
+            this.lblUpcomingEvents.TabIndex = 71;
+            this.lblUpcomingEvents.Text = "Upcoming events:";
+            // 
+            // SeeEventsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 706);
+            this.Controls.Add(this.lblUpcomingEvents);
+            this.Controls.Add(this.btnChangeDate5);
+            this.Controls.Add(this.btnTitleThree6);
+            this.Controls.Add(this.btnSeeMoreThree6);
+            this.Controls.Add(this.btnTitleTwo6);
+            this.Controls.Add(this.btnSeeMoreTwo6);
+            this.Controls.Add(this.btnTitleOne6);
+            this.Controls.Add(this.btnSeeMoreOne6);
+            this.Controls.Add(this.btnMainPage6);
+            this.Controls.Add(this.lblTitle6);
+            this.Name = "SeeEventsForm";
+            this.Text = "SeeEventsForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChangeDate5;
+        private System.Windows.Forms.Button btnTitleThree6;
+        private System.Windows.Forms.Button btnSeeMoreThree6;
+        private System.Windows.Forms.Button btnTitleTwo6;
+        private System.Windows.Forms.Button btnSeeMoreTwo6;
+        private System.Windows.Forms.Button btnTitleOne6;
+        private System.Windows.Forms.Button btnSeeMoreOne6;
+        private System.Windows.Forms.Button btnMainPage6;
+        private System.Windows.Forms.Label lblTitle6;
+        private System.Windows.Forms.Label lblUpcomingEvents;
+    }
+}
