bd8fe8bf775a060d48acdfd2605ab73039ddbdfa
prima-c <prima.changwatchai@gmail.com>
2025-02-07 10:17:14 -0600

    Finished change-password page design.

---
+++ DailyPlanner/ChangePasswordForm.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace DailyPlanner
+{
+    partial class ChangePasswordForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPwdOld8 = new System.Windows.Forms.TextBox();
+            this.btnChangePwd8 = new System.Windows.Forms.Button();
+            this.txtUser8 = new System.Windows.Forms.TextBox();
+            this.lblPwdOld8 = new System.Windows.Forms.Label();
+            this.lblUser8 = new System.Windows.Forms.Label();
+            this.lblEnterPwdInst8 = new System.Windows.Forms.Label();
+            this.lblTitle8 = new System.Windows.Forms.Label();
+            this.txtPwdNew8 = new System.Windows.Forms.TextBox();
+            this.lblPwdNew8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPwdOld8
+            // 
+            this.txtPwdOld8.Location = new System.Drawing.Point(42, 409);
+            this.txtPwdOld8.Name = "txtPwdOld8";
+            this.txtPwdOld8.Size = new System.Drawing.Size(312, 20);
+            this.txtPwdOld8.TabIndex = 31;
+            // 
+            // btnChangePwd8
+            // 
+            this.btnChangePwd8.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnChangePwd8.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChangePwd8.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnChangePwd8.Location = new System.Drawing.Point(42, 565);
+            this.btnChangePwd8.Name = "btnChangePwd8";
+            this.btnChangePwd8.Size = new System.Drawing.Size(312, 45);
+            this.btnChangePwd8.TabIndex = 30;
+            this.btnChangePwd8.Text = "Change Password";
+            this.btnChangePwd8.UseVisualStyleBackColor = false;
+            // 
+            // txtUser8
+            // 
+            this.txtUser8.Location = new System.Drawing.Point(42, 310);
+            this.txtUser8.Name = "txtUser8";
+            this.txtUser8.Size = new System.Drawing.Size(312, 20);
+            this.txtUser8.TabIndex = 29;
+            // 
+            // lblPwdOld8
+            // 
+            this.lblPwdOld8.AutoSize = true;
+            this.lblPwdOld8.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPwdOld8.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblPwdOld8.Location = new System.Drawing.Point(37, 365);
+            this.lblPwdOld8.Name = "lblPwdOld8";
+            this.lblPwdOld8.Size = new System.Drawing.Size(221, 30);
+            this.lblPwdOld8.TabIndex = 28;
+            this.lblPwdOld8.Text = "Old Password:";
+            // 
+            // lblUser8
+            // 
+            this.lblUser8.AutoSize = true;
+            this.lblUser8.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUser8.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblUser8.Location = new System.Drawing.Point(37, 267);
+            this.lblUser8.Name = "lblUser8";
+            this.lblUser8.Size = new System.Drawing.Size(157, 30);
+            this.lblUser8.TabIndex = 27;
+            this.lblUser8.Text = "Username:";
+            // 
+            // lblEnterPwdInst8
+            // 
+            this.lblEnterPwdInst8.AutoSize = true;
+            this.lblEnterPwdInst8.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnterPwdInst8.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblEnterPwdInst8.Location = new System.Drawing.Point(95, 180);
+            this.lblEnterPwdInst8.Name = "lblEnterPwdInst8";
+            this.lblEnterPwdInst8.Size = new System.Drawing.Size(221, 60);
+            this.lblEnterPwdInst8.TabIndex = 26;
+            this.lblEnterPwdInst8.Text = "Enter your\r\nnew password.";
+            // 
+            // lblTitle8
+            // 
+            this.lblTitle8.AutoSize = true;
+            this.lblTitle8.Font = new System.Drawing.Font("Courier New", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle8.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblTitle8.Location = new System.Drawing.Point(0, 96);
+            this.lblTitle8.Name = "lblTitle8";
+            this.lblTitle8.Size = new System.Drawing.Size(399, 50);
+            this.lblTitle8.TabIndex = 25;
+            this.lblTitle8.Text = "Daily Planner";
+            // 
+            // txtPwdNew8
+            // 
+            this.txtPwdNew8.Location = new System.Drawing.Point(42, 511);
+            this.txtPwdNew8.Name = "txtPwdNew8";
+            this.txtPwdNew8.Size = new System.Drawing.Size(312, 20);
+            this.txtPwdNew8.TabIndex = 33;
+            // 
+            // lblPwdNew8
+            // 
+            this.lblPwdNew8.AutoSize = true;
+            this.lblPwdNew8.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPwdNew8.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblPwdNew8.Location = new System.Drawing.Point(37, 467);
+            this.lblPwdNew8.Name = "lblPwdNew8";
+            this.lblPwdNew8.Size = new System.Drawing.Size(221, 30);
+            this.lblPwdNew8.TabIndex = 32;
+            this.lblPwdNew8.Text = "New Password:";
+            // 
+            // ChangePasswordForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 706);
+            this.Controls.Add(this.txtPwdNew8);
+            this.Controls.Add(this.lblPwdNew8);
+            this.Controls.Add(this.txtPwdOld8);
+            this.Controls.Add(this.btnChangePwd8);
+            this.Controls.Add(this.txtUser8);
+            this.Controls.Add(this.lblPwdOld8);
+            this.Controls.Add(this.lblUser8);
+            this.Controls.Add(this.lblEnterPwdInst8);
+            this.Controls.Add(this.lblTitle8);
+            this.Name = "ChangePasswordForm";
+            this.Text = "ChangePasswordForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPwdOld8;
+        private System.Windows.Forms.Button btnChangePwd8;
+        private System.Windows.Forms.TextBox txtUser8;
+        private System.Windows.Forms.Label lblPwdOld8;
+        private System.Windows.Forms.Label lblUser8;
+        private System.Windows.Forms.Label lblEnterPwdInst8;
+        private System.Windows.Forms.Label lblTitle8;
+        private System.Windows.Forms.TextBox txtPwdNew8;
+        private System.Windows.Forms.Label lblPwdNew8;
+    }
+}
