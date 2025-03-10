59b45740d224bbb1c91cfead933f260469a5b51c
primac2025 <prima.c2025@gmail.com>
2025-03-09 20:40:06 -0500

    Added code to update event.

--- DailyPlanner/EventForm.cs
+++ DailyPlanner/EventForm.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace DailyPlanner
 {
@@ -13,6 +14,7 @@ namespace DailyPlanner
 
         List<Event> events = new List<Event>();
         string SelectedValue = null;
+        string NameToEdit = null;
 
         public EventForm(string SelectedValue)
         {
@@ -33,27 +35,78 @@ namespace DailyPlanner
         }
 
         private void SetControls()
-        {            
-            //Populate Combo Boxes - To Do
+        {
+            //Populate Combo Boxes
 
+            Debug.WriteLine("Set controls " + SelectedValue.ToString());
+
+            NameToEdit = SelectedValue.ToString();
 
             //Populate the Controls
             var selected = (from ev in events
                             where ev.title == SelectedValue.ToString()
                             select ev).ToList();
-            
+
             if (selected.Count > 0)
             {
                 this.txtTitle.Text = selected[0].title;
-                //this.cboType.SelectedText = selected[0].type;
+                this.cboType.SelectedText = selected[0].type;
                 this.txtDate.Text = selected[0].date;
-                //this.cboStatus.SelectedText= selected[0].status;
+                this.cboStatus.SelectedText = selected[0].status;
                 this.txtDesc.Text = selected[0].description;
             }            
         }
 
         private void btnUpdateEvent9_Click(object sender, EventArgs e)
         {
+
+            //var selected = (from ev in events
+            //                where ev.title == SelectedValue.ToString()
+            //                select ev).ToList();
+
+            //Event event = from ev in events
+            //              where ev.title == SelectedValue.ToString()
+            //              select ev;
+
+
+            string file = CurrentPath.GetDatabasePath() + "\\" + "EventData.txt";
+
+            // Write all but old event
+            events = Event.GetEvents();
+            events.RemoveAll(ev => ev.title == NameToEdit);
+
+            System.IO.File.Delete(file);
+
+            string prefix = "";
+
+            foreach (Event ev in events)
+            {
+                Debug.WriteLine(ev.title);
+                
+                string eventString = $"{prefix}{ev.title}|{ev.type}|{ev.date}|{ev.status}|{ev.description}";
+                System.IO.File.AppendAllText(file, eventString);
+                prefix = "\n";
+            }
+
+            // Add event
+
+            string title = txtTitle.Text;
+            string type = cboType.SelectedText;
+            string date = txtDate.Text;
+            string status = cboStatus.SelectedText;
+            string description = txtDesc.Text;
+
+            string addedEvent = $"\n{title}|{type}|{date}|{status}|{description}";
+            System.IO.File.AppendAllText(file, addedEvent);
+
+            Debug.WriteLine("UPdate " + SelectedValue.ToString());
+            Debug.WriteLine("B " + NameToEdit);
+
+            Debug.WriteLine("A " + txtTitle.Text);
+
+
+            //selected[0].title = this.txtTitle.Text;
+
             //Close current form
             this.Close();
             //Create a thread to RUN a NEW application with the desired form
